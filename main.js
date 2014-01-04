fd5578a2f1518557f3eef22319cd744dffdb4a3e
Soledad Penades <sole@mozilla.com>
2014-01-04 20:12:50 +0100

    the evil bug-it's here!

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 var previewContainer = document.getElementById('preview');
+var mosaicContainer = document.getElementById('mosaic');
+var shooter;
 
-GumHelper.startVideoStreaming(function(error, stream, videoElement) {
+GumHelper.startVideoStreaming(function(error, stream, videoElement, width, height) {
 	if(error) {
 		// TODO
 		return;
 	}
 
+	videoElement.width = width / 4;
+	videoElement.height = height / 4;
+
 	previewContainer.appendChild(videoElement);
 	
+	shooter = new VideoShooter(videoElement);
+
 	startCapturing();
+
 });
 
 function startCapturing() {
+
+	console.log('start capturing, current number of images ', mosaicContainer.childElementCount);
+	if(mosaicContainer.childElementCount === 9) {
+	//	debugger;
+	}
+
+	shooter.getShot(function onDone(pictureData) {
+		console.log('ta-da');
+		var img = document.createElement('img');
+		img.src = pictureData;
+		mosaicContainer.appendChild(img);
+
+		setTimeout(startCapturing, 100);
+	
+	}, 1, 0.2, function onProgress(progress) {
+		console.log('done ', progress);
+	});
 
 }
---
+++ videoShooter.js
@@ -0,0 +1,46 @@
+//define(['Animated_GIF'], function (Animated_GIF) {
+//  'use strict';
+
+  function VideoShooter (videoElement) {
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    context.scale(-1, 1); // mirror flip preview back to the normal direction
+
+    canvas.width = videoElement.width;
+    canvas.height = videoElement.height;
+
+    this.getShot = function (callback, numFrames, interval, progressCallback) {
+      numFrames = numFrames !== undefined ? numFrames : 3;
+      interval = interval !== undefined ? interval : 0.1; // In seconds
+
+      var pendingFrames = numFrames;
+      var ag = new Animated_GIF({ workerPath: 'libs/Animated_GIF.worker.js' });
+      ag.setSize(canvas.width, canvas.height);
+      ag.setDelay(interval);
+
+      captureFrame();
+
+      function captureFrame() {
+        ag.addFrame(videoElement);
+        pendingFrames--;
+
+        // Call back with an r value indicating how far along we are in capture
+        progressCallback((numFrames - pendingFrames) / numFrames);
+
+        if(pendingFrames > 0) {
+          setTimeout(captureFrame, interval * 1000); // timeouts are in milliseconds
+        } else {
+          ag.getBase64GIF(callback);
+          //ag.getBase64GIF(function(image) {
+            //var img = document.createElement('img');
+            //img.src = image;
+            //document.body.appendChild(img);
+            //callback(image);
+          //});
+        }
+      }
+    };
+  }//;
+
+//  return VideoShooter;
+//});
